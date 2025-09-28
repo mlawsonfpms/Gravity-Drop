e1c1934a85977f3e815cad5a53ef797a6f806539
mlawsonfpms <mlawson@ecsdfl.us>
2025-09-28 13:51:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,111 +1,109 @@
-const gravities = {
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = 500;
+
+const gravityValues = {
   Earth: 9.8,
   Moon: 1.6,
   Mars: 3.7,
   Jupiter: 24.8,
-  Mercury: 3.7,
-  Venus: 8.87,
-  Saturn: 10.4,
-  Uranus: 8.69,
-  Neptune: 11.2,
-  Pluto: 0.62
+  Europa: 1.3
 };
 
 const objects = {
-  Rock: { mass: 1, airCoeff: 0.1 },
-  Feather: { mass: 0.1, airCoeff: 1.2 },
-  Cannonball: { mass: 5, airCoeff: 0.05 }
+  Feather: { img: "feather.png", mass: 0.02 },
+  "Bowling Ball": { img: "bowlingball.png", mass: 6 }
 };
 
-function populateOptions() {
-  const planetSelects = [document.getElementById('planet1'), document.getElementById('planet2')];
-  const objectSelects = [document.getElementById('object1'), document.getElementById('object2')];
+const planetOptions = Object.keys(gravityValues);
+const objectOptions = Object.keys(objects);
 
-  for (let planet in gravities) {
-    planetSelects.forEach(sel => {
-      const opt = document.createElement('option');
-      opt.value = planet;
-      opt.textContent = planet;
-      sel.appendChild(opt);
-    });
-  }
-
-  for (let obj in objects) {
-    objectSelects.forEach(sel => {
-      const opt = document.createElement('option');
-      opt.value = obj;
-      opt.textContent = obj;
-      sel.appendChild(opt);
-    });
-  }
+function populateSelect(id, options) {
+  const sel = document.getElementById(id);
+  sel.innerHTML = "";
+  options.forEach(opt => {
+    const o = document.createElement("option");
+    o.value = opt;
+    o.textContent = opt;
+    sel.appendChild(o);
+  });
 }
 
-let animationFrame;
-let startTime;
-let positions = [0, 0];
-let velocities = [0, 0];
-let finished = [false, false];
+populateSelect("planet1", planetOptions);
+populateSelect("planet2", planetOptions);
+populateSelect("object1", objectOptions);
+populateSelect("object2", objectOptions);
 
-function startDrop() {
-  const [planet1, planet2] = [document.getElementById('planet1').value, document.getElementById('planet2').value];
-  const [object1, object2] = [document.getElementById('object1').value, document.getElementById('object2').value];
-  const [air1, air2] = [document.getElementById('air1').checked, document.getElementById('air2').checked];
+class DropObject {
+  constructor(x, planet, object, air) {
+    this.x = x;
+    this.y = 0;
+    this.vy = 0;
+    this.planet = planet;
+    this.gravity = gravityValues[planet];
+    this.object = object;
+    this.mass = objects[object].mass;
+    this.air = air;
+    this.img = new Image();
+    this.img.src = objects[object].img;
+    this.done = false;
+  }
 
-  const g1 = gravities[planet1];
-  const g2 = gravities[planet2];
-  const obj1 = objects[object1];
-  const obj2 = objects[object2];
-
-  const objEls = [document.getElementById('objectA'), document.getElementById('objectB')];
-  objEls[0].style.left = '35%';
-  objEls[1].style.left = '55%';
-
-  velocities = [0, 0];
-  positions = [0, 0];
-  finished = [false, false];
-
-  startTime = null;
-
-  function animate(timestamp) {
-    if (!startTime) startTime = timestamp;
-    const elapsed = (timestamp - startTime) / 1000;
-
-    [g1, g2].forEach((g, i) => {
-      if (finished[i]) return;
-      const obj = i === 0 ? obj1 : obj2;
-      const air = i === 0 ? air1 : air2;
-      let acc = g;
-      if (air) acc -= obj.airCoeff * velocities[i];
-      velocities[i] += acc * 0.02;
-      positions[i] += velocities[i] * 2;
-
-      if (positions[i] >= 340) {
-        positions[i] = 340;
-        finished[i] = true;
-      }
-
-      objEls[i].style.top = `${positions[i]}px`;
-    });
-
-    document.getElementById('info').textContent =
-      `${planet1} gravity: ${g1} m/s² | ${planet2} gravity: ${g2} m/s²\nTime: ${elapsed.toFixed(2)} s`;
-
-    if (!finished[0] || !finished[1]) {
-      animationFrame = requestAnimationFrame(animate);
+  update(dt) {
+    if (this.done) return;
+    let g = this.gravity;
+    if (this.air) {
+      const drag = 0.1 * this.vy;
+      this.vy += (g - drag) * dt;
+    } else {
+      this.vy += g * dt;
+    }
+    this.y += this.vy;
+    if (this.y >= canvas.height - 50) {
+      this.y = canvas.height - 50;
+      this.done = true;
     }
   }
 
-  animationFrame = requestAnimationFrame(animate);
+  draw() {
+    ctx.drawImage(this.img, this.x - 25, this.y, 50, 50);
+  }
 }
 
-function reset() {
-  cancelAnimationFrame(animationFrame);
-  document.getElementById('objectA').style.top = '0px';
-  document.getElementById('objectB').style.top = '0px';
-  document.getElementById('info').textContent = '';
-  velocities = [0, 0];
-  positions = [0, 0];
-  finished = [false, false];
-}
+let drops = [];
+let time = 0;
+let interval = null;
 
-populateOptions();
+document.getElementById("dropButton").onclick = () => {
+  drops = [];
+  time = 0;
+  const p1 = planet1.value;
+  const p2 = planet2.value;
+  const o1 = object1.value;
+  const o2 = object2.value;
+  const a1 = air1.checked;
+  const a2 = air2.checked;
+  drops.push(new DropObject(canvas.width / 3, p1, o1, a1));
+  drops.push(new DropObject((2 * canvas.width) / 3, p2, o2, a2));
+  interval = setInterval(update, 20);
+};
+
+document.getElementById("resetButton").onclick = () => {
+  clearInterval(interval);
+  drops = [];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  document.getElementById("info").textContent = "";
+};
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "lime";
+  ctx.fillRect(0, canvas.height - 2, canvas.width, 2);
+  time += 0.02;
+  drops.forEach(d => {
+    d.update(0.02);
+    d.draw();
+  });
+  document.getElementById("info").textContent = `${drops[0].planet} gravity: ${drops[0].gravity} m/s² | ${drops[1].planet} gravity: ${drops[1].gravity} m/s²\nTime: ${time.toFixed(2)} s`;
+}
